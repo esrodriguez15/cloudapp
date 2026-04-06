cf386f691176e831d06664c5912f638a23c9ffbc
Esmeralda <erodriguez280@my.gcu.edu>
2026-04-05 22:09:06 -0400

    for Heroku deployment

---
+++ src/main/java/com/cloudapp/cloudapp/configuration/WebConfig.java
@@ -0,0 +1,20 @@
+package com.cloudapp.cloudapp.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.cloudapp.cloudapp.interceptor.LoggingInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoggingInterceptor loggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
+}
---
+++ src/main/java/com/cloudapp/cloudapp/interceptor/LoggingInterceptor.java
@@ -0,0 +1,29 @@
+
+package com.cloudapp.cloudapp.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class LoggingInterceptor implements HandlerInterceptor {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        logger.info("[ENTER] {} {}", request.getMethod(), request.getRequestURI());
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+            Exception ex) {
+        if (ex != null) {
+            logger.error("[EXCEPTION] {} - {}", ex.getClass().getSimpleName(), ex.getMessage());
+        }
+        logger.info("[EXIT] {} {}", request.getMethod(), request.getRequestURI());
+    }
+
+}
