fd35c00b71671dac3df8cf93323dc45ca22373ba
Esmeralda <erodriguez280@my.gcu.edu>
2026-04-05 22:27:03 -0400

    CloudAppApplication.java change

--- src/main/java/com/cloudapp/cloudapp/interceptor/LoggingInterceptor.java
+++ src/main/java/com/cloudapp/cloudapp/interceptor/LoggingInterceptor.java
@@ -3,11 +3,13 @@ package com.cloudapp.cloudapp.interceptor;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class LoggingInterceptor implements HandlerInterceptor {
     private static final Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
 
